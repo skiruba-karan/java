efa0b5c0b220be522d049c7bfd31b7a57a868a1d
Kiruba Karan <kirubakaranatwork@gmail.com>
2025-02-07 12:55:09 +0530

    day 6

--- wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
+++ wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
@@ -11,5 +11,20 @@ private final Calculator calculator = new Calculator();
 		int result = calculator.add(3,2);
 		assertEquals(5,result);
 	}
-
+	@Test
+	public void testSubtract() {
+		int result = calculator.subtract(5,3);
+		assertEquals(2,result);
+	}
+	@Test
+	public void testAddWithNegativeNumbers() {
+		int result = calculator.add(-3, -2);
+		assertEquals(-5,result);
+	}
+	
+	@Test
+	public void testSubtractWithNegativeNumbers() {
+		int result = calculator.subtract(-3, -2);
+		assertEquals(-1,result);
+	}
 }
